889af0a5b1a297e6a056ccff3b2b7d5a3690ac40
Weng Kexin <42372486+Weng-Kexin@users.noreply.github.com>
2020-10-31 00:56:48 +0800

    new waveform.py

--- PyHipp/waveform.py
+++ PyHipp/waveform.py
@@ -47,16 +47,12 @@ class Waveform(DPT.DPObject):
         # 'channelxxx, xxx is the number of the channel'
 
         pwd = os.path.normpath(os.getcwd());
-        '''
         aname = DPT.levels.normpath(os.path.dirname(pwd))
         
         self.array_dict = dict()
         self.array_dict[aname] = 0
         self.numSets = 1
         self.current_plot_type = None
-                
-        '''
-        
         
         self.channel_filename = [os.path.basename(pwd)]  
         template_filename = os.path.join(
@@ -81,6 +77,11 @@ class Waveform(DPT.DPObject):
         # It is useful to store the information of the objects for panning through in the future
         DPT.DPObject.append(self, wf)  # append self.setidx and self.dirs
         self.data = self.data + wf.data
+        for ar in wf.array_dict:
+            self.array_dict[ar] = self.numSets
+        self.numSets += 1
+
+        
         
     def plot(self, i = None, ax = None, getNumEvents = False, getLevels = False,\
              getPlotOpts = False, overlay = False, **kwargs):
