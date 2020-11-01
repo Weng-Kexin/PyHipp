049f3f3d3d014475964cef1e01a3eda7af5227a1
Weng Kexin <42372486+Weng-Kexin@users.noreply.github.com>
2020-11-01 22:44:55 +0800

    h

--- PyHipp/t.py
+++ PyHipp/t.py
@@ -1,6 +1,11 @@
 import DataProcessingTools as DPT
 import matplotlib.pyplot as plt
 
+import hickle as hkl
+import os
+import numpy as np
+
+
 class Waveform(DPT.DPObject):
     # Please change the class name according to your needs
     filename = 'waveform.hkl'  # this is the filename that will be saved if it's run with saveLevel=1
@@ -13,36 +18,24 @@ class Waveform(DPT.DPObject):
     def create(self, *args, **kwargs):
         # this function will be called once to create this waveform object
         
-        # one neat property of Object-Oriented Programming (OOP) structure is that 
-        # you can create some field-value pairs that can be called and updated 
-        # in all functions of the object, if you specify the function properly.
-        # The only thing that you need to do is to instantiate those fields in
-        # this function with the prefix 'self.', then you can call them and 
-        # edit them in all the other functions that have the first input argument
-        # being 'self'
-        #
-        # For exmample, if you instantiate a field-value pair:
-        # self.name = IronMan
-        #
-        # You can then call them or edit them in other functions:
-        # def get_name(self):
-        #    print(self.name)
-        #
-        # def set_name(self, new_name):
-        #    self.name = new_name
-        #
-        # In this way, you don't need to return and pass in so many arguments 
-        # across different functions anymore :)
         
         
-        # The following is some hints of the things-to-do:
+        pwd = os.path.normpath(os.getcwd());
+        # 'channelxxx, xxx is the number of the channel'
+        self.channel_filename = [os.path.basename(pwd)]  
+        template_filename = os.path.join(
+            DPT.levels.resolve_level('day', self.channel_filename[0]),
+            'mountains', self.channel_filename[0], 'output', 'templates.hkl')
+        templates = hkl.load(template_filename)
+        self.data = [np.squeeze(templates)]
         
-        # read the mountainsort template files
-        # .........................................
-        # ..................code...................
-        # .........................................
+        aname = DPT.levels.normpath(os.path.dirname(pwd))        
+        self.array_dict = dict()
+        self.array_dict[aname] = 0
+        self.numSets = 1
+        self.current_plot_type = None
         
-        
+
         # check on the mountainsort template data and create a DPT object accordingly
         # Example:
         if self.data != []:
@@ -58,7 +51,10 @@ class Waveform(DPT.DPObject):
         # It is useful to store the information of the objects for panning through in the future
         DPT.DPObject.append(self, wf)
         self.data = self.data + wf.data
-    
+        for ar in wf.array_dict:
+            self.array_dict[ar] = self.numSets
+        self.numSets += 1
+
         
     def plot(self, i = None, ax = None, getNumEvents = False, getLevels = False,\
              getPlotOpts = False, overlay = False, **kwargs):
